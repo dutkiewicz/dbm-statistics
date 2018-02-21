2bf0591907745ff2c7f3088e82cbf449e4c4e28f
Dawid Dutkiewicz <dawid.dutkiewicz@gmail.com>
2018-02-21 16:10:01 +0100

    - refactored code to use DBMQuery methods instead of functions - added purging metadata of csv reports when commiting to DB (mostly it was info about report's date and filters at the end) -

--- dbm_api/cli.py
+++ dbm_api/cli.py
@@ -6,8 +6,7 @@ import requests
 from decouple import config
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from util import DBMQuery, run_query, list_queries, create_query, download_query, delete_query, \
-    clean_date_value, clean_money_value
+from util import DBMQuery, clean_date_value, clean_money_value
 from models import BasicStats, Base
 
 # parse arguments from CLI
@@ -17,7 +16,7 @@ parser.add_argument('-l', '--list', action='store_true', help='List available qu
 parser.add_argument('-d', '--download-report', type=int, nargs=1, help='Download report file based on query ID')
 parser.add_argument('-s', '--save-report', type=int, nargs=1, help='Save CSV file to database. ' \
                                                                                'Requires preconfigured SQL database!')
-parser.add_argument('-r', '--run-query', type=int, nargs=1, help='Run query with data from previous day.')
+parser.add_argument('-r', '--run-query', nargs=2, help='Run query ID (1) with data from date range (2).')
 parser.add_argument('-c', '--create-query', type=str, nargs=1, help='Create new Query from JSON file')
 parser.add_argument('--remove-query', nargs='*', help='Delete queries from DBM')
 args = parser.parse_args()
@@ -25,17 +24,17 @@ args = parser.parse_args()
 DIR = os.path.dirname(os.path.abspath(__file__))
 
 if os.path.exists(args.api_key):
-    dbm = DBMQuery(args.api_key).authorize()
+    dbm = DBMQuery(args.api_key)
 else:
     raise OSError("API key {} does not exist".format(args.api_key))
 
 if args.list:
-    list_queries(dbm)
+    dbm.list_queries()
 
 if args.download_report:
     print("Downloading Query {}".format(args.download_report))
 
-    query = requests.get(download_query(args.download_report[0], dbm))
+    query = requests.get(dbm.download_query(args.download_report[0]))
     filename = str(args.download_report[0]) + '.csv'
 
     print("Saving file...")
@@ -44,7 +43,7 @@ if args.download_report:
     print("File saved!")
 
 if args.save_report:
-    query = requests.get(download_query(args.save_report[0], dbm))
+    query = requests.get(dbm.download_query(args.save_report[0], dbm))
     filename = str(args.save_report[0]) + '.csv'
 
     # save report data to SQL
@@ -61,31 +60,34 @@ if args.save_report:
         report = csv.DictReader(file)
 
         for counter, row in enumerate(report, 1):
-            record = BasicStats(date = clean_date_value(row["Date"]),
-                                advertiser = row["Advertiser"],
-                                advertiser_id = row["Advertiser ID"],
-                                insertion_order = row["Insertion Order"],
-                                insertion_order_id = row["Insertion Order ID"],
-                                line_item = row["Line Item"],
-                                line_item_id = row["Line Item ID"],
-                                currency = row["Advertiser Currency"],
-                                impressions = row['Impressions'],
-                                viewable_impressions = row["Active View: Viewable Impressions"],
-                                clicks = row['Clicks'],
-                                total_conversions = row["Total Conversions"],
-                                total_cost = clean_money_value(row["Total Media Cost (Advertiser Currency)"]))
-            session.add(record)
-            session.commit()
+            if len(row["Date"]) == 0: # DBM csv reports have meta info at the bottom of file. We have to skip them
+                break
+            else:
+                record = BasicStats(date = clean_date_value(row["Date"]),
+                                    advertiser = row["Advertiser"],
+                                    advertiser_id = row["Advertiser ID"],
+                                    insertion_order = row["Insertion Order"],
+                                    insertion_order_id = row["Insertion Order ID"],
+                                    line_item = row["Line Item"],
+                                    line_item_id = row["Line Item ID"],
+                                    currency = row["Advertiser Currency"],
+                                    impressions = row['Impressions'],
+                                    viewable_impressions = row["Active View: Viewable Impressions"],
+                                    clicks = row['Clicks'],
+                                    total_conversions = row["Total Conversions"],
+                                    total_cost = clean_money_value(row["Total Media Cost (Advertiser Currency)"]))
+                session.add(record)
+                session.commit()
 
             print("Saved row {}".format(counter))
 
 if args.run_query:
-    run_query(args.run_query[0], dbm)
+    dbm.run_query(args.run_query[0], args.run_query[1])
 
 if args.create_query:
-    create_query(args.create_query[0], dbm)
+    dbm.create_query(args.create_query[0])
 
 if args.remove_query:
     for id in args.remove_query:
-        delete_query(id, dbm)
+        dbm.delete_query(id)
         print("Removed Query ID {}".format(id))
